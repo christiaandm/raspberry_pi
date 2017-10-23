a48b8ba561cb8b21920a0fbde64130eaf9fffc35
Soufian <info@souf.nl>
2017-10-23 10:44:10 +0200

    Testing

---
+++ send.py
@@ -0,0 +1,11 @@
+#source: http://www.mrhobbytronics.com/raspberry-pi-communicating-between-pis/
+import socket
+UDP_IP = "CHANGE TO PI'S IP"
+UDP_Port = 5005
+MESSAGE = "Hello, World!"
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+print("message:", MESSAGE)
+sock = socket.socket(socket.AF_INET, # Internet
+socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
